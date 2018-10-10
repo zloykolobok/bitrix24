83fc81c9a424584d5ed792a1f0fb5cd5cac50a03
Roman Nikolaenkov <rnikolaenkov@yandex.ru>
2018-10-10 14:10:49 +0300

    Added Disk

---
+++ src/Classes/Disk.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zloykolobok\Bitrix24\Classes;
+
+use Zloykolobok\Bitrix24\Bitrix;
+
+class Disk extends Bitrix
+{
+    public function uploadFile(int $id, array $fields, string $content)
+    {
+        $action = 'disk.storage.uploadfile.json';
+        $data['ID'] = $id;
+        $data['fields'] = $fields;
+        $data['fileContent'] = $content;
+
+        $res = $this->send($data,$action);
+
+        return $res;
+    }
+
+}
