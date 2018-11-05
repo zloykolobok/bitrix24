26b9b9074c2d97efc51602d32ef573012b5042ce
Roman Nikolaenkov <rnikolaenkov@yandex.ru>
2018-11-05 22:24:49 +0300

    updated activity

--- src/Classes/Activity.php
+++ src/Classes/Activity.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: romchik
- * Date: 05.11.2018
- * Time: 18:48
- */
 
 namespace Zloykolobok\Bitrix24\Classes;
 
@@ -12,6 +6,12 @@ use Zloykolobok\Bitrix24\Bitrix;
 
 class Activity extends Bitrix
 {
+    /**
+     * Создаёт новое дело.
+     *
+     * @param array $fields
+     * @return mixed
+     */
     public function activityAdd(array $fields)
     {
         $action = 'crm.activity.add.json';
@@ -26,7 +26,7 @@ class Activity extends Bitrix
      * Возвращает список активностей по фильтру.
      * (Для получения COMMUNICATIONS его надо явно указать в select.)
      * Является реализацией списочного метода для активностей.
-     * 
+     *
      * @param array $order
      * @param array $filter
      * @param array $select
@@ -40,10 +40,92 @@ class Activity extends Bitrix
      */
     public function activityList(array $order, array $filter, array $select)
     {
-        $action = 'crm.activity.list';
+        $action = 'crm.activity.list.json';
         $data['order'] = $order;
         $data['filter'] = $filter;
         $data['select'] = $select;
+
+        $res = $this->send($data,$action);
+
+        return $res;
+    }
+
+    /**
+     * Возвращает описание коммуникации для активности.
+     * Коммуникации хранят номера телефонов в звонках,
+     * email-адреса в письмах, имена во встречах.
+     *
+     * @return mixed
+     */
+    public function activityCommunicationFields()
+    {
+        $action = 'crm.activity.communication.fields.json';
+        $data = [];
+
+        $res = $this->send($data,$action);
+
+        return $res;
+    }
+
+    /**
+     * Удаляет активность.
+     *
+     * @param int $id
+     * @return mixed
+     */
+    public function activityDelete(int $id)
+    {
+        $action = 'crm.activity.delete.json';
+        $data['id'] = $id;
+
+        $res = $this->send($data,$action);
+
+        return $res;
+    }
+
+    /**
+     * Возвращает описание полей активности.
+     *
+     * @return mixed
+     */
+    public function activityFields()
+    {
+        $action = 'crm.activity.fields.json';
+        $data = [];
+
+        $res = $this->send($data,$action);
+
+        return $res;
+    }
+
+    /**
+     * Возвращает активность по идентификатору.
+     *
+     * @param int $id
+     * @return mixed
+     */
+    public function activityGet(int $id)
+    {
+        $action = 'crm.activity.get.json';
+        $data['id'] = $id;
+
+        $res = $this->send($data,$action);
+
+        return $res;
+    }
+
+    /**
+     * Обновляет существующую активность.
+     *
+     * @param int $id
+     * @param array $fields
+     * @return mixed
+     */
+    public function activityUpdate(int $id, array $fields)
+    {
+        $action = 'crm.activity.update.json';
+        $data['id'] = $id;
+        $data['fields'] = $fields;
 
         $res = $this->send($data,$action);
 
